80a7a76020fca9aa823e492513e2124326126159
Leonardo Da Vinci <84631921+leanardo7994@users.noreply.github.com>
2022-06-18 18:24:18 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -177,11 +177,11 @@ async def send_text(client: Bot, message: Message):
         
         status = f"""<b><u>Broadcast Completed</u>
 
-Total Users: <code>{total}</code>
-Successful: <code>{successful}</code>
-Blocked Users: <code>{blocked}</code>
-Deleted Accounts: <code>{deleted}</code>
-Unsuccessful: <code>{unsuccessful}</code></b>"""
+◉ Total Users: <code>{total}</code>
+◉ Successful: <code>{successful}</code>
+◉ Blocked Users: <code>{blocked}</code>
+◉ Deleted Accounts: <code>{deleted}</code>
+◉ Unsuccessful: <code>{unsuccessful}</code></b>"""
         
         return await pls_wait.edit(status)
 
